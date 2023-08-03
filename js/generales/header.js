df90359c8c984230e33d942231f4690ded49d99b
Mauricio Picca <mpicca83@hotmail.com>
2023-08-03 16:06:44 -0300

    Corrección error 2

--- js/generales/header.js
+++ js/generales/header.js
@@ -1,5 +1,6 @@
 // Creación de Header de forma dinamica por DOM
 
+let ruta = ".."
 const header = document.querySelector('.header')
 
     //Encabezado parte superior
@@ -37,35 +38,35 @@ const headerContenedorInferior = document.createElement('NAV')
 headerContenedorInferior.classList.add('navbar', 'navbar-expand-lg', 'encabezadoInferior')
 headerContenedorInferior.innerHTML = `
     <div class="container-fluid">
-        <a class="navbar-brand" href="/uberwinden/index.html"><img src="/uberwinden/assets/images/logoUberwinden.png" alt="Logo Überwinden" id="logoMarca"/></a>
+        <a class="navbar-brand" href="${ruta}/index.html"><img src="../assets/images/logoUberwinden.png" alt="Logo Überwinden" id="logoMarca"/></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="./uberwinden/index.html">Inicio</a>
+                    <a class="nav-link" aria-current="page" href="${ruta}/index.html">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/uberwinden/pages/nosotros.html">Nosotros</a>
+                    <a class="nav-link" href="${ruta}/pages/nosotros.html">Nosotros</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item fw-bold" href="/uberwinden/pages/indumentaria_paravos.html">Para vos...</a></li>
-                        <li><a class="dropdown-item ps-5" href="/uberwinden/pages/productos.html?categoria=Hombre">Hombre</a></li>
-                        <li><a class="dropdown-item ps-5" href="/uberwinden/pages/productos.html?categoria=Mujer">Mujer</a></li>
+                        <li><a class="dropdown-item fw-bold" href="${ruta}/pages/indumentaria_paravos.html">Para vos...</a></li>
+                        <li><a class="dropdown-item ps-5" href="${ruta}/pages/productos.html?categoria=Hombre">Hombre</a></li>
+                        <li><a class="dropdown-item ps-5" href="${ruta}/pages/productos.html?categoria=Mujer">Mujer</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item fw-bold" href="/uberwinden/pages/personaliza_tu_indumentaria.html">Para tu club...</a></li>
-                        <li><a class="dropdown-item ps-5" href="/uberwinden/pages/en_construccion.html">Merchandising</a></li>
-                        <li><a class="dropdown-item ps-5" href="/uberwinden/pages/personaliza_tu_indumentaria.html">Personaliza tu indumentaria</a></li>
+                        <li><a class="dropdown-item fw-bold" href="${ruta}/pages/personaliza_tu_indumentaria.html">Para tu club...</a></li>
+                        <li><a class="dropdown-item ps-5" href="${ruta}/pages/en_construccion.html">Merchandising</a></li>
+                        <li><a class="dropdown-item ps-5" href="${ruta}/pages/personaliza_tu_indumentaria.html">Personaliza tu indumentaria</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/uberwinden/pages/eventos.html">Eventos</a>
+                    <a class="nav-link" href="${ruta}/pages/eventos.html">Eventos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/uberwinden/pages/login.html"><i class="fa-solid fa-user"></i></a>
+                    <a class="nav-link" href="${ruta}/pages/login.html"><i class="fa-solid fa-user"></i></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal" href=""><i class="fa-sharp fa-solid fa-cart-shopping"><span id="iconoCarrito"></span></i></a>
@@ -82,14 +83,20 @@ headerContenedorInferior.innerHTML = `
 
     // Para obtener la ruta actual de la página
 const rutaActual = window.location.pathname
-
     // Para obtener todos los enlaces (etiqueta a)
 const enlaces = headerContenedorInferior.querySelectorAll('a')
 
+
 enlaces.forEach((enlace) => {
     const href = enlace.getAttribute('href')
+
     if (href === rutaActual) {
         enlace.classList.add('active')
+    }
+
+    let ruta = ".."
+    if (rutaActual === "/index.html"){
+        ruta = "."
     }
 })
 
