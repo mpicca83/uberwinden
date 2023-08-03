050b6ed46e6afa45d626d3f58a35cec3fe57f5d3
Mauricio Picca <mpicca83@hotmail.com>
2023-08-03 17:10:07 -0300

    Actualización de links

--- js/generales/footer.js
+++ js/generales/footer.js
@@ -1,4 +1,9 @@
 // Creación de Footer de forma dinamica por DOM
+let ruta = ".."
+
+if (document.title == "Überwinden - Ropa deportiva") {
+    ruta = "."
+}
 
 const footer = document.querySelector('.footer')
 
@@ -7,29 +12,29 @@ footerContenedor.classList.add('footer_Contenedor')
 footerContenedor.innerHTML = `
     <ul class="tiendaOnline">
         <li><h5>Tienda online</h5></li>
-        <li><a href="/pages/productos.html?categoria=Hombre">Hombre</a></li>
-        <li><a href="/pages/productos.html?categoria=Mujer">Mujer</a></li>
-        <li><a href="/pages/en_construccion.html">Merchandising clubes</a></li>
-        <li><a href="/pages/personaliza_tu_indumentaria.html">Personaliza tu indumentaria</a></li>
+        <li><a href="${ruta}/pages/productos.html?categoria=Hombre">Hombre</a></li>
+        <li><a href="${ruta}/pages/productos.html?categoria=Mujer">Mujer</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Merchandising clubes</a></li>
+        <li><a href="${ruta}/pages/personaliza_tu_indumentaria.html">Personaliza tu indumentaria</a></li>
     </ul>
     <ul class="ayuda">
         <li><h5>Ayuda</h5></li>
-        <li><a href="/pages/en_construccion.html">Preguntas frecuentes</a></li>
-        <li><a href="/pages/en_construccion.html">Politica de envíos</a></li>
-        <li><a href="/pages/en_construccion.html">Cambio y devoluciones</a></li>
-        <li><a href="/pages/en_construccion.html">Medios de pago</a></li>
-        <li><a href="/pages/en_construccion.html">Cancelación de compra</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Preguntas frecuentes</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Politica de envíos</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Cambio y devoluciones</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Medios de pago</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Cancelación de compra</a></li>
     </ul>
     <ul class="acercaDe">
         <li><h5>Acerca de Überwinden</h5></li>
-        <li><a href="/pages/nosotros.html">Quines somos</a></li>
-        <li><a href="/pages/eventos.html">Eventos</a></li>
-        <li><a href="/pages/en_construccion.html">Tiendas</a></li>
+        <li><a href="${ruta}/pages/nosotros.html">Quines somos</a></li>
+        <li><a href="${ruta}/pages/eventos.html">Eventos</a></li>
+        <li><a href="${ruta}/pages/en_construccion.html">Tiendas</a></li>
     </ul>
     <ul class="usuario">
         <li><h5>Usuario</h5></li>
-        <li><a href="/pages/login.html">Iniciar sesión</a></li>
-        <li><a href="/pages/registro.html">Registrate</a></li>
+        <li><a href="${ruta}/pages/login.html">Iniciar sesión</a></li>
+        <li><a href="${ruta}/pages/registro.html">Registrate</a></li>
     </ul>
     <ul class="iconos">
         <li><a href="https://www.instagram.com/uberwindensportwear/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
